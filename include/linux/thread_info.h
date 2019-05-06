5c1e465b691c1b15641009453d8720988f860832
utsavbalar1231 <utsavbalar1231@gmail.com>
2019-05-06 04:08:06 +0000

    Revert "UPSTREAM: thread_info: include <current.h> for THREAD_INFO_IN_TASK"
    This reverts commit bec762d115b69c0867970e1e5959737f8beaad35.
    
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>

--- include/linux/thread_info.h
+++ include/linux/thread_info.h
@@ -12,12 +12,17 @@
 #include <linux/restart_block.h>
 
 #ifdef CONFIG_THREAD_INFO_IN_TASK
-/*
- * For CONFIG_THREAD_INFO_IN_TASK kernels we need <asm/current.h> for the
- * definition of current, but for !CONFIG_THREAD_INFO_IN_TASK kernels,
- * including <asm/current.h> can cause a circular dependency on some platforms.
- */
-#include <asm/current.h>
+struct thread_info {
+	u32			flags;		/* low level flags */
+};
+
+#define INIT_THREAD_INFO(tsk)			\
+{						\
+	.flags		= 0,			\
+}
+#endif
+
+#ifdef CONFIG_THREAD_INFO_IN_TASK
 #define current_thread_info() ((struct thread_info *)current)
 #endif
 
